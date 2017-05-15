ef513888000cbe87a5f2be555bbd9d0cf7f5fab2
Brad Watson <brad.watson.orlando@gmail.com>
2017-05-15 10:39:16 -0400

    Done.

---
+++ components/DumbComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class DumbComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mood: 'happy'
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.state.mood === 'happy' ? this.setState({mood: 'sad'}) : this.setState({mood: 'happy'})
+  }
+  render() {
+    return (
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    );
+  }
+}
---
+++ components/DumberComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function DumberComponent(props) {
+  return (
+    <div id="dumber" onClick={props.handleClick}>I am just happy</div>
+  );
+}
