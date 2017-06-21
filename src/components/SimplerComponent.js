33444c881943b79959319f959e063d14f12bb750
Phreemason <github email address>
2017-06-21 18:55:02 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,14 @@
 // Code SimplerComponentHere Here
+import React from 'react';
+
+const defaultLimit = 100
+
+const SimplerComponent = (props)=>{
+  return(
+  	<div onClick={props.handleClick}>
+      I am just happy
+    </div>
+  );
+}
+
+export default SimplerComponent;
