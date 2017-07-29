e488c32eb2858821a6e78d1a7581a6520c952721
Trai Compton <tlcompton2546@outlook.com>
2017-07-29 00:58:49 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,31 @@
-// Code SimpleComponentHere Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+    constructor(){
+        super();
+        
+        this.state = {
+            mood: 'happy'
+        };
+        
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick(){
+        var moodChange = this.state.mood === 'happy' ? 'sad' : 'happy';
+        
+        this.setState({
+            mood: moodChange
+        });
+    }
+    
+    render() {
+        return (
+            <div onClick={ this.handleClick }>
+                { this.state.mood }
+            </div>
+        );
+    }
+}
+
+export default SimpleComponent;
