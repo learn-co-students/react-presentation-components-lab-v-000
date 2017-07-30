3f1658c74c74bf9c7266f4163a40c09cf48fd847
David Kennell <davidkennell0@gmail.com>
2017-07-30 18:11:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const displaySublime = () => {
+	return(
+		<div>Hello</div>
+	)
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.displaySublime} />
   </div>,
   document.getElementById('root')
 );
