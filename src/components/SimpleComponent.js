1014937ec0ffe96e62b58d6b7360ff2b0ebbe670
Andrew Bonner <andrewbonner92@gmail.com>
2017-08-05 13:01:24 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,27 @@
-// Code SimpleComponentHere Here
+import React, {Component} from 'react';
+
+class SimpleComponent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mood: 'happy'
+    }
+  }
+
+  changeMood = () => {
+    this.setState({
+      mood: 'sad'
+    })
+  }
+
+  render() {
+    return(
+      <div onClick={this.changeMood}>
+        <p>{this.state.mood}</p>
+      </div>
+    )
+  }
+}
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const toggleMood = () => alert("You just made me sad!")
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={toggleMood} />
   </div>,
   document.getElementById('root')
 );
