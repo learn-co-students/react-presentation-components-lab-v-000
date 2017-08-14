22fbbb5936c11a0e6d6cdb75d5d8abc05e4e7235
Isis Tejeda <isis@isistejeda.com>
2017-08-14 11:24:13 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,14 @@
-// Code SimplerComponentHere Here
+import React from 'react';
+import { Component } from 'react';
+
+const mood = 'happy'
+
+ const SimplerComponent = (props) => {
+    return <div onClick={props.handleClick}>
+        I am just happy
+
+    </div>
+}
+ export default SimplerComponent;
+
+
