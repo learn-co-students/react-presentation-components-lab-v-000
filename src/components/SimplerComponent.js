9b127a9b56cad2b30709c77f852d1ef8df88ff08
J Fernandez <jffernan@comcast.net>
2017-09-21 19:23:56 +0000

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,7 @@
-// Code SimplerComponentHere Here
+import React from 'react';
+
+const DumberComponent = (props) => {
+  return <div onClick={props.handleClick}>I am just happy.</div>;
+};
+
+export default DumberComponent;
