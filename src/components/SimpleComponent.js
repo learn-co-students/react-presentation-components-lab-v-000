782e3b7930aff242dcb80d4c8a1b27b56e7931d2
christinechoi <smi77y@gmail.com>
2017-10-15 18:24:09 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponentHere Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      mood: this.props.mood
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div>{this.props.mood}</div>
+    )
+  }
+
+}
+
+
+SimpleComponent.defaultProps = {
+  mood: "happy"
+};
+
+export default SimpleComponent;
