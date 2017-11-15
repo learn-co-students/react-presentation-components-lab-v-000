ddcd92b100dc6f20cd35425c141e60e1263009f4
WilliamMena <williammena1991@gmail.com>
2017-11-14 21:31:47 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,37 @@
 // Code SimpleComponentHere Here
+import React from 'react';
+class SimpleComponentHere extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mood: 'happy'
+    };
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return (this.state !== nextState);
+  // }
+
+  // componentWillUpdate(nextProps, nextState) {
+  //   return console.log(this.state)
+  // }
+
+  handleClick = () => {
+    if (this.state.mood === 'happy') {
+      this.setState({
+      mood: 'sad' }, () => console.log(this.state))
+    } else (
+      this.setState({
+      mood: 'happy' }, () => console.log(this.state))
+    )
+    // in here I need to change the state to sad or back to happy
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>
+
+  }
+
+}
+export default SimpleComponentHere;
