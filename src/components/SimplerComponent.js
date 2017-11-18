de7d9a7e21caead214a1d6ed06206b122c66dc16
vanitanaidu <vanita112003@gmail.com>
2017-11-18 15:35:31 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SimplerComponent = props => {
   return(
-    <div onClick={props.handleClick}>"I am just happy"</div>
+    <div onClick={props.handleClick}>I am just happy</div>
   )
 }
 
