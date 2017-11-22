1da6ebd5e6bfa697d239c917d6ba5ad61697f79c
Bruce Molina <bruce.molina.81@gmail.com>
2017-11-22 03:53:13 +0000

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,20 @@
-// Code SimplerComponentHere Here
+// // Code SimplerComponentHere Here
+// import React from 'react'
+// const defaultMood = 'I am just happy'
+//
+//
+// const SimplerComponent = (props) =>
+//     <div>
+//       {defaultMood}
+//       <button onClick={props.handleClick}>{defaultMood}</button>
+//     </div>
+//
+//
+// export default SimplerComponent;
+
+import React from 'react';
+
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>;
+
+export default SimplerComponent;
