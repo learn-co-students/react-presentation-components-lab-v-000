b9a540140e196d5719b09a7c9b8f26cf7ed2a957
NickisKnowledge <lifechangingstuff@gmail.com>
2017-12-31 15:18:29 -0600

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,24 @@
+import React, { Component } from 'react';
 // Code SimplerComponentHere Here
+
+const SimplerComponent = prop => {
+  return (
+    <div className='simpler-component' onClick={prop.handleClick}>
+      I am just happy
+    </div>
+  );
+}
+
+SimplerComponent.defaultProps = {
+  handleClick() {
+    if (document.querySelector('.simpler-component').innerHTML ===
+      'I am just happy') {
+      document.querySelector('.simpler-component').innerHTML = 'Now, I am sad';
+    } else {
+      document.querySelector('.simpler-component').innerHTML =
+        'I am just happy';
+    }
+  },
+};
+
+export default SimplerComponent;
