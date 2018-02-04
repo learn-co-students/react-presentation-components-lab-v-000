56c3c5cbc900c3d5b013398b4ec069291067e548
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-04 22:25:19 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const shoutHappy = () => {
+  alert("Happyy happyy happyy happyy!")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={shoutHappy} />
   </div>,
   document.getElementById('root')
 );
