9680fca9243a6ea45a3867ae455a0f2efea502bc
emorgan05 <emorgan05@gmail.com>
2018-02-16 19:06:19 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
-const turnRed = (event) => {
+const always = (event) => {
   event.target.innerHTML = "I am always happy"
 }
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={turnRed} />
+    <SimplerComponent handleClick={always} />
   </div>,
   document.getElementById('root')
 );
