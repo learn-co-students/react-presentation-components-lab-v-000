602138d889a366d315c64c65cf6fdd48d6e00c0e
Hannah Carnes <hecarnes@gmail.com>
2018-04-04 19:04:37 -0400

    has state

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,17 @@
 // Code SimpleComponentHere Here
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.mood}</h1>
+      </div>
+    );
+  }
+}
