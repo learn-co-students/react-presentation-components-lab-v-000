90bf298d7fb35bff987ca78d5b5e645db84b1e2e
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-27 21:35:46 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponentHere Here
+import React from 'react'
+
+export default class SimpleComponentHere extends React.Component {
+  constructor() {
+  super()
+
+  this.state = {
+    mood: 'happy'
+    }
+  }
+
+  handleChange = () => {
+    this.setState({
+      mood: 'sad'
+    })
+  }
+  render() {
+    return (
+      <div onClick={this.handleChange}>
+        {this.state.mood}
+      </div>
+    )
+  }
+}
