74d61fc2abc787ad9d204a508c81ee4e5a7559ed
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-05-11 15:41:48 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,35 @@
 // Code SimpleComponentHere Here
+import React, { Component} from 'react';
+
+class SimpleComponent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mood: "happy"
+    }
+  }
+
+  handleClick = () => {
+    if (this.state.mood === "happy") {
+    this.setState({
+      mood: "sad",
+      color: "blue"
+    })} else {
+      this.setState({
+        mood: "happy",
+        color: "red"
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div style={{fontWeight: 'bold', fontSize: 30, color: this.state.color}} onClick={this.handleClick}>
+        {this.state.mood}
+      </div>
+    )
+  }
+}
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const changeColor = (event) => {
+  if (event.target.style.color === "red") {
+    event.target.style.color = "blue";
+  } else {
+    event.target.style.color = "red";
+  }
+}
+
 ReactDOM.render(
   <div>
-    <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimpleComponent /><br />
+    <SimplerComponent handleClick={changeColor} />
   </div>,
   document.getElementById('root')
 );
