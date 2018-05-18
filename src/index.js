e52d5cc9f5d6df1588f2d68a26e476c9649be946
ingridwong0715 <ingrid830715@gmail.com>
2018-05-18 11:44:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={ () => alert("I'm happy!")} />
   </div>,
   document.getElementById('root')
 );
