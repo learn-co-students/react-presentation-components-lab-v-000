ca961a69c2ab0bd659ff2f89d15ec7044cf2fe35
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-06 14:21:08 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,26 @@
 // Code SimplerComponentHere Here
+
+import React from 'react'
+
+const SimplerComponent = (props) => {
+  return (
+    <div onClick={props.handleClick}>
+      "I am just happy"
+    </div>
+
+  )
+}
+
+
+// This passes all but the last test:
+
+// const SimplerComponent = ({property}) => {
+//   return (
+//     <div onClick={property}>
+//       "I am just happy"
+//     </div>
+//
+//   )
+// }
+
+export default SimplerComponent
