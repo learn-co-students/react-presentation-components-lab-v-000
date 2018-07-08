b710f893b49e724d0daf652218f50ef567151440
scliff6262 <sclifford6262@gmail.com>
2018-07-08 13:22:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const whenClicked = (e) => {
+  e.persist()
+  if(!!e.target.innerText.search("happy")){
+    e.target.innerText = 'I am just sad.'
+  } else if(!e.target.innerText.search("happy")) {
+    e.target.innerText = 'I am just happy.'
+  }
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={whenClicked} />
   </div>,
   document.getElementById('root')
 );
