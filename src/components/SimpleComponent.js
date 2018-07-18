a54d07ca9eaa5384f05850151d6644dbf3ec060a
BuffaloRob <peckham.rob@gmail.com>
2018-07-18 10:21:31 -0400

    done

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,30 @@
 // Code SimpleComponentHere Here
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class SimpleComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mood: 'happy'
+    };
+  }
+
+
+  handleClick = () => {
+    const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    this.setState({
+      mood: 'sad'
+    })
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        {this.state.mood}
+      </div>
+    );
+  }
+}
+
+export default SimpleComponent;
