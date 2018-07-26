ee0e46f9eb44a7dab22a7a2ef16f14e86ac1e08e
TylerP33 <tyler3321@gmail.com>
2018-07-26 14:05:35 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
-// Code SimpleComponentHere Here
+import React, { Component } from 'react';
+import SimplerComponent from './SimplerComponent'
+import Toggle from 'react-toggle'
+
+export default class SimpleComponent extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			mood: 'happy'
+		}
+	}
+
+	handleClick = () => {
+		const toggle = this.state.mood === 'happy' ? 'sad' : 'happy'
+		this.setState({
+			mood: toggle
+		})
+	}
+
+
+	render() {
+		return(
+			<div onClick={this.handleClick}>
+			<p>I am {this.state.mood}</p>
+			</div>
+			)
+	}
+}
