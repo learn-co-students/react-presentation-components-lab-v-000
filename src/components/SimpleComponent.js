a5461f56781508c6919c36e02108bd6bed799a0f
Emily Jirles <emily.jirles@gmail.com>
2018-08-03 18:38:31 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,26 @@
 // Code SimpleComponent Here
+import React from 'react';
+
+export default class SimpleComponent extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mood: 'happy'
+    }
+  }
+
+  handleClick = () => {
+    switch (this.state.mood) {
+      case "happy":
+        this.setState({mood: 'sad'})
+        break;
+      case "sad":
+        this.setState({mood: 'happy'})
+        break;
+    }
+  }
+
+  render(){
+    return <div onClick={this.handleClick}>{this.state.mood}</div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert('SOOOO HAPPY')} />
   </div>,
   document.getElementById('root')
 );
