31878f3aee01ea46fa1f55998b4677e2c5a11b93
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-08-23 19:05:57 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,17 @@
+import React from 'react';
+
 // Code SimplerComponent Here
+
+// const TextField = (props) =>
+//   <input
+//     className="field field-light"
+//     onChange={props.onChange}
+//     limit={props.limit || defaultLimit}
+//   />;
+
+const SimplerComponent = (props) =>
+  <div onClick={props.handleClick}>
+    I am just happy
+  </div>
+
+export default SimplerComponent
