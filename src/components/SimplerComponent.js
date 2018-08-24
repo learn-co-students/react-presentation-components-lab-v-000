83d0ae3b89004e6882e1a2a861a5131a8278e42b
fonzmp <fonzmp@gmail.com[color]>
2018-08-24 14:08:52 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,9 @@
-// Code SimplerComponent Here
+import React from "react";
+
+const SimplerComponent = ({ handleClick }) => (
+  <div onClick={handleClick}>I am just happy</div>
+);
+
+const handleClick = () => console.log("Clicked!");
+
+export default SimplerComponent;
