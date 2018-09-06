27ab19affa04a0e0bafb41fe5c2e53bf9e5c0d92
smithwebtek <brad@smithwebtek.com>
2018-09-06 14:35:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+// function handleClick2() {
+// 	alert("this function was passed as a prop from index.js")
+// }
+
 ReactDOM.render(
-  <div>
-    <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
-  </div>,
-  document.getElementById('root')
+
+	<div>
+		<SimpleComponent />
+		{/* <SimplerComponent handleClick={() => handleClick2()} */}
+		<SimplerComponent handleClick={undefined}
+		/>
+	</div>,
+	document.getElementById('root')
 );
