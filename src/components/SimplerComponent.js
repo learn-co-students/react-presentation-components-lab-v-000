410a6146f5391c5969a49c8aaa87e8f2f39adac0
Vanessa Toro <vanetoro84@gmail.com>
2018-09-13 20:05:05 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -2,13 +2,5 @@
 import React, {Component} from 'react';
  
 const SimplerComponent = (props) =>  <div onClick={props.handleClick}>I am just happy.</div>
-// class SimplerComponent extends Component{
-
-//     render(){
-//         return(
-           
-//         )
-//     }
-// }
 
 export default SimplerComponent;
