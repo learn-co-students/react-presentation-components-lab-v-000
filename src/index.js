2faa28ed54713024a1031d270db98c4ccc7fc214
Tori Machen <tmachen@pillartechnology.com>
2018-10-13 17:37:17 -0500

    added simple and simpler presentational components

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert('I am sad now..')} />
   </div>,
   document.getElementById('root')
 );
