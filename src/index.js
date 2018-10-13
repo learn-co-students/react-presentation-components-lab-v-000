e148df03e052a7c9791b952a5d1bb0abd17008c8
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-10-12 18:32:01 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const onClick = (event) => console.log("happier");
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={onClick} />
   </div>,
   document.getElementById('root')
 );
