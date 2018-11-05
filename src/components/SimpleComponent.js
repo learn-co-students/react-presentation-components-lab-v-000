7cd48ac1c755732c0af3e411a249cf2c20952fe3
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-05 19:00:38 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,24 @@
 // Code SimpleComponent Here
+import React, {PureComponent} from 'react'
+ export default class SimpleComponent extends PureComponent {
+   constructor() {
+    super()
+    this.state = {
+      mood: "happy"
+    }
+  }
+   handleClick = () => {
+    if (this.state.mood === "happy") {
+      this.setState({
+        mood: "sad"
+      })
+    } else {
+      this.setState({
+        mood: "happy"
+      })
+    }
+  }
+   render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>
+  }
+}
