e9bd65f6566590570520065a63b8b49f644165e4
terryblue99 <terryblue@comcast.net>
2019-02-07 19:17:47 -0600

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
-// Code SimpleComponent Here
+import React from 'react'
+
+// 1. The component should be declared as a class (instance of Component) so that it can carry state.
+// 2. The component should have a state property called mood that has a default value of happy.
+// 3. The component should simply render its current mood state to the page in a div.
+// 4. The component should implement a handleClick function that can serve as a callback to the <div>'s 
+//    click event. When clicked, the component's mood should toggle between happy and sad states.
+
+class SimpleComponent extends React.Component {
+
+  	constructor() {
+    	super()
+ 
+    	this.state = {
+      		mood: 'happy'
+    	}
+  	}
+
+  	handleClick = () => { 
+		const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    	this.setState({ mood: newMood });
+  	}
+
+  	render() {
+	    return <div onClick={this.handleClick}>{this.state.mood}</div>
+  	}
+}
+
+export default SimpleComponent
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,15 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+// 1. The component should be a "stateless functional" component.
+// 2. It should render a <div> to the page that contains the text: "I am just happy".
+// 3. It should receive one property called handleClick that performs some sort of action 
+//    in response to a click!
+
+const SimplerComponent = (props) => {
+	return <div onClick={props.handleClick}>I am just happy.</div>	
+}
+
+export default SimplerComponent
+
+// ALTERNATIVE:
+// const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>
