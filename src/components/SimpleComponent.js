432cb5e1d7bebef449980943fbabd52a52070419
Kachel Wittig <kjwittig@gmail.com>
2019-02-15 12:14:55 -0600

    Donezo

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,17 @@
-// Code SimpleComponent Here
+import React from 'react'
+
+class SimpleComponent extends React.Component {
+  state = {
+    mood: 'happy',
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>
+  }
+
+  handleClick = e => {
+    this.setState({ mood: 'sad' })
+  }
+}
+
+export default SimpleComponent
