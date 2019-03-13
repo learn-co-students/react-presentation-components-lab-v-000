7d6347b668b08c37c39b5e15fa6708c5703109bb
monksandninjas <github email address>
2019-03-13 17:51:03 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,27 @@
+import React, { Component } from 'react'
 // Code SimpleComponent Here
+
+class SimpleComponent  extends Component {
+
+  state = {
+    mood: 'happy'
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        {this.state.mood}
+      </div>
+    )
+  }
+
+  handleClick = () => {
+    const mood = this.state.mood === 'happy' ? 'sad' : 'happy';
+
+    this.setState ({
+      mood: mood
+    });
+  }
+}
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import SimplerComponent from './components/SimplerComponent';
 
 ReactDOM.render(
   <div>
+
     <SimpleComponent />
     <SimplerComponent handleClick={undefined} />
   </div>,
