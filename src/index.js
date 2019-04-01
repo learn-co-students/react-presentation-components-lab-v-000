09528f409e44af14000da9bdd0efd5d52c5e538f
Bruce Hillsworth <brucehillsworth@Bruces-iMac.local>
2019-04-01 16:06:44 -0600

    Passed Tests

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleClick = () => {
+  console.log('I am actually sad')
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
