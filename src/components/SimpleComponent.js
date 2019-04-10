df5f85d8223cfc5cd096d3d90e5edf161d3aaabc
github username <github email address>
2019-04-09 18:30:18 -0700

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+
+    state = {
+        mood: 'happy'
+    }
+
+    handleClick(event) {
+        this.setState(prevState => ({
+            mood: prevState.mood === 'happy' ? 'sad' : 'happy'
+          }));
+    }
+
+    render() {
+        return (
+            <div onClick={event => this.handleClick(event)}>{this.state.mood}</div>
+        );
+      }
+
+}
+export default SimpleComponent
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const sayHello = () => {
+  alert("Hello")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.sayHello} />
   </div>,
   document.getElementById('root')
 );
