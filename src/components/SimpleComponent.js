0d694e65cbdd0159b07b3a65d8e3c3c99ef0e346
Rafael Lopez <lpz.raf@gmail.com>
2019-09-07 09:49:24 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -4,15 +4,31 @@ import React, { Component } from 'react'
 
 export default class SimpleComponent extends Component {
 
-    state = {
-        mood: "happy"
+    constructor(props) {
+        super(props);
+        this.state = {
+            mood: "happy"
+        }
     }
 
-    handleClick() {
-        this.setState=({
-            mood: "sad" 
-        })
-    }
+
+
+
+    handleClick(){
+        this.state.mood === 'happy' ? this.setState({mood: 'sad'}) : this.setState({mood: 'happy'})
+    
+    //     if (newMood === 'happy') {
+    //     this.setState = ({
+    //         mood: 'sad'
+    //     });
+    //   } else {
+    //         this.setState = ({
+    //             mood: 'happy'
+    //   })
+      }
+
+
+
     render() {
         return (
             <div onClick={this.handleClick.bind(this)}>
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,4 +1,11 @@
-import { readFileSync } from "fs";
-
-// Code SimplerComponent Here
-readFileSync
+import React from 'react'
+ 
+const SimplerComponent = props => {
+  return (
+    <div onClick={props.handleClick}>
+    I am just happy.
+    </div>
+  )
+}
+ 
+export default SimplerComponent
