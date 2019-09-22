19fe6a1d1cfdb4c0357a35fd6c91808f34b7a6c7
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-22 12:36:48 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleClick = () => {
+  //not in class so define /w const
+  console.log('not!')
+
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
