a7852a5f13facebc0ceb6745dacac6c9c9e38424
BusterStatus <matthew.serre@gmail.com>
2019-10-04 19:43:33 -0700

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,20 @@
 // Code SimplerComponent Here
+
+import React from 'react';
+
+
+
+const SimplerComponent = function() {
+
+    const handleClick = () => {
+        console.log("Really happy")
+      }
+
+    return (
+        <div onClick={console.log("Balls")}>
+            I am just happy
+        </div>
+    )
+}
+
+export default SimplerComponent
