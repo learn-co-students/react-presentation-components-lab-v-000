5cd701d427fe5506d9d85ba34d4c1de9651bf39e
github username <github email address>
2019-11-06 16:12:13 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={function myFunction() {
+  alert("Hello! I am an alert box!");
+}} />
   </div>,
   document.getElementById('root')
 );
