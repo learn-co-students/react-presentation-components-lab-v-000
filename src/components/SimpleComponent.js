fea85dd0aaf4c315ef3144e839378fabd55126b2
github username <github email address>
2019-11-13 16:48:16 -0800

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,20 @@
-// Code SimpleComponent Here
+import React, { Component } from "react";
+
+class SimpleComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initial State here..
+    this.state = { mood: "I am just happy" };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>{this.state.mood}</h3>
+      </div>
+    );
+  }
+}
+
+export default SimpleComponent;
