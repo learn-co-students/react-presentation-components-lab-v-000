b35d3e26e15857ddc7b1e71326489083addfa63a
Nate Dietz <nsdietz@gmail.com>
2019-12-30 20:15:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => {<div>HAPPY, HAPPY, HAPPY!!!</div>}} />
   </div>,
   document.getElementById('root')
 );
