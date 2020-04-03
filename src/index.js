bd4d8241edb82c33fba57dd407cd77645d8a228e
Richard Cho <richardcho100@gmail.com>
2020-04-03 02:32:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleClick = (event) => {
+  event.persist()
+  console.log(event.target.textContent)
+} 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
+
