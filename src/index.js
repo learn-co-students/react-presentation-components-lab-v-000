c336e4cd6e0a871b8ff4b5d068682de16e521289
Sara Harvey <saraharveynyc@gmail.com>
2020-12-02 12:15:16 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const changeColor = (event) => {
+  event.target.style.color === "red" ? event.target.style.color = "black" : event.target.style.color = "red"
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
