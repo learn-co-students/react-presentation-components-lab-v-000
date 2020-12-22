41020e7e4f45146dabddba2105bc7e5f2577fb4b
RonSala <ron.sala@gmail.com>
2020-12-21 21:46:07 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+function SimplerComponent() {
+  return (
+    <div>
+      <p>I am just happy</p>
+    </div>
+  )
+}
+
+export default SimplerComponent
