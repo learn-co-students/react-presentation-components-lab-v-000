a172a6c41a374ded42a00613b07924cd2b2fc17f
klovae <elyse.klova@gmail.com>
2021-06-07 17:51:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log('But is anyone ever just happy?') } />
   </div>,
   document.getElementById('root')
 );
